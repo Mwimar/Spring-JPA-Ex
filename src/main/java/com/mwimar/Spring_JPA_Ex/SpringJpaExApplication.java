faa4ec6311c9683a8f049f08d0c0e02b229145fc
Mark Mwima <markmwimar@gmail.com>
2024-09-13 12:59:36 +0300

    app context

--- src/main/java/com/mwimar/Spring_JPA_Ex/SpringJpaExApplication.java
+++ src/main/java/com/mwimar/Spring_JPA_Ex/SpringJpaExApplication.java
@@ -2,12 +2,15 @@ package com.mwimar.Spring_JPA_Ex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringJpaExApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringJpaExApplication.class, args);
+
+		ApplicationContext context = SpringApplication.run(SpringJpaExApplication.class, args);
+
 	}
 
 }
