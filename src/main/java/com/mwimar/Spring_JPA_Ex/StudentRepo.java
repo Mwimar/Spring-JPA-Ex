68be057eb877a1b27e6b05079170509d3b23168c
Mark Mwima <markmwimar@gmail.com>
2024-09-13 13:11:37 +0300

    extending student repo to spring JPA

--- src/main/java/com/mwimar/Spring_JPA_Ex/StudentRepo.java
+++ src/main/java/com/mwimar/Spring_JPA_Ex/StudentRepo.java
@@ -1,4 +1,9 @@
 package com.mwimar.Spring_JPA_Ex;
 
-public interface StudentRepo {
+import com.mwimar.Spring_JPA_Ex.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepo extends JpaRepository <Student, Integer> {
 }
