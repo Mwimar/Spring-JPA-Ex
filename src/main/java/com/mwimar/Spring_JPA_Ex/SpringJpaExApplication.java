5562108e9f494ef6673be58b24a8142f89094fbe
Mark Mwima <markmwimar@gmail.com>
2024-09-13 13:03:07 +0300

    student objects

--- src/main/java/com/mwimar/Spring_JPA_Ex/SpringJpaExApplication.java
+++ src/main/java/com/mwimar/Spring_JPA_Ex/SpringJpaExApplication.java
@@ -1,5 +1,6 @@
 package com.mwimar.Spring_JPA_Ex;
 
+import com.mwimar.Spring_JPA_Ex.model.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -10,6 +11,11 @@ public class SpringJpaExApplication {
 	public static void main(String[] args) {
 
 		ApplicationContext context = SpringApplication.run(SpringJpaExApplication.class, args);
+
+		Student s1 = context.getBean(Student.class);
+		Student s2 = context.getBean(Student.class);
+		Student s3 = context.getBean(Student.class);
+		Student s4 = context.getBean(Student.class);
 
 	}
 
