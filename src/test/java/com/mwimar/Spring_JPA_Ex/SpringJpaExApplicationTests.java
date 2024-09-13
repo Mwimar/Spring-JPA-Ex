69a9187a66795a59bbede7564aca494c171c130d
Mark Mwima <markmwimar@gmail.com>
2024-09-13 12:58:23 +0300

    spring JPA

---
+++ src/test/java/com/mwimar/Spring_JPA_Ex/SpringJpaExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mwimar.Spring_JPA_Ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
